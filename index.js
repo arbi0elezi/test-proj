15f14921d46abf302e3e53348a1044964530b2d9
Arbi Elezi <aelezi@ritech.co>
2020-01-14 11:45:57 +0100

    adding template libs

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const express = require('express');
 const app = express();
 
 const testCircular = () => {
-let obj1 = {
-    text: "I am obj1"
-};
-let obj2 = {
-    text: "I am obj2"
-};
+	let obj1 = {
+		text: "I am obj1"
+	};
+	let obj2 = {
+		text: "I am obj2"
+	};
 
-obj1.obj = obj2;
-obj2.obj = obj1;
+	obj1.obj = obj2;
+	obj2.obj = obj1;
 
 
-console.log(obj1);
-console.log(obj1.obj);
-console.log(obj2.obj.obj.obj.obj.obj);
+	console.log(obj1);
+	console.log(obj1.obj);
+	console.log(obj2.obj.obj.obj.obj.obj);
 };
 
 const mid = (req, res, next) => {
@@ -29,29 +29,31 @@ const mid = (req, res, next) => {
 
 const mid1 = (req, res, next) => {
 	console.log('I am mid 1');
-	console.log(req.query);	
+	console.log(req.query);
 	res.send('Hello');
 	next();
 }
 
 const mid2 = (req, res, next) => {
 	console.log('I am mid 2');
-//	res.send('World');
+	//	res.send('World');
 	next();
 }
 
 const mid3 = (req, res, next) => {
 	console.log('I am mid 3');
-//	console.log(res);
-	console.log(req.params);	
-//res.send('Howdy');
+	//	console.log(res);
+	console.log(req.params);
+	//res.send('Howdy');
 	next();
 }
 
 app.get('/mid/:id', [mid2, mid3, mid1])
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
 	res.send('hello!!');
 });
+
+
 
 app.listen(3131);
